bf78fb53ecc3a040a48edd1b74e51b7d8d5c8c2d
Bobo <60228056+OneSadRatard@users.noreply.github.com>
2021-09-25 00:11:38 +0100

    added input

--- index.js
+++ index.js
@@ -1,21 +1,39 @@
-const mineflayer = require('mineflayer')
+const readline = require("readline");
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+rl.question("IP: ", function(ip) {
+    rl.question("Email: ", function(email) {
+	rl.question("Password: ", function(password){ 
+        console.log('Wait...')
+		
+		const mineflayer = require('mineflayer')
 
 const bot = mineflayer.createBot({
-  host: 'server ip', // minecraft server ip
-  username: 'email', // minecraft username
-  password: 'password' // minecraft password, comment out if you want to log into online-mode=false servers
+  host: `${ip}`, // minecraft server ip
+  username: `${email}`, // minecraft username
+  password: `${password}` // minecraft password, comment out if you want to log into online-mode=false servers
+
 })
+
 
 bot.on('playerLeft', function(player) {
   bot.chat("bye " + player.username + " IMPERIALS ON TOP");
 });
 
 bot.on('login', function() {
-  console.log("I logged in.");
-  console.log("settings", bot.settings);
+  console.log("I logged into " + `${ip}` + '.');
+  
 });	
 
 // Log errors and kick reasons:
 bot.on('kicked', console.log)
 bot.on('error', console.log)
-	
+
+        
+    });
+   });
+});
+ 
